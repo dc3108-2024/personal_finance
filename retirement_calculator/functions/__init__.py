0054249af1fa261d05dc6584c2af57973a05cd5d
dc3108-2024 <dc3108.debashis@gmail.com>
2025-02-15 23:47:27 +1100

    add optimise contribution functionality as an endpoint in itself

--- retirement_calculator/functions/__init__.py
+++ retirement_calculator/functions/__init__.py
@@ -3,3 +3,4 @@ from .calcTargetIncome import calculate_inflation_adjusted_income
 from .calculateAccumulation import calculate_yearly_buildup
 from .calculateAnnuity import calculate_annuity_payments
 from .calculateNPV import calculate_npv
+from .optimiseContribution import optimise_contribution
